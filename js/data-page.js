8767fa39adf11057ba37293c6d992b2848e9de5b
Andre <ncl8@mail.ru>
2022-02-06 11:27:44 +0300

    lesson 4.4

---
+++ js/data-page.js
@@ -0,0 +1,16 @@
+import {theme} from './theme.js';
+
+theme.coolUpdate();
+
+window.addEventListener('storage', function (event) {
+    theme.coolUpdate();
+});
+
+$(function(){
+    var includes = $('[data-include]');
+    //document.querySelector("[data-include]").innerText;
+    jQuery.each(includes, function(){
+        var file = 'components/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
